2ab7a443f7e9de3004152c80f74e8fd99075f503
linguochi <382150914@qq.com>
2016-12-02 13:57:15 +0800

    配置环境fis.media

--- src/fis-conf.js
+++ src/fis-conf.js
@@ -5,10 +5,16 @@ fis.match('*', {
 });
 //html文件放在static根目录下
 fis.match('/views/(**)', {
-    release:'/static/$1'
+    release: '/static/$1'
 });
 //解析下less
-fis.match('/less/**.less',{
+fis.match('/less/**.less', {
     parser: fis.plugin('less'),
     rExt: '.css'
 });
+
+/** 配置环境**/
+fis.media('prod')
+    .match('*.{less,css}', {
+        optimizer: fis.plugin('clean-css') // 压缩CSS
+    })
