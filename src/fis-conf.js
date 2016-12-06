86317e9ec78d99ee7d0ea3ef93b8baa34a40ca67
linguochi <382150914@qq.com>
2016-12-06 11:15:45 +0800

    打包

--- src/fis-conf.js
+++ src/fis-conf.js
@@ -29,38 +29,38 @@ fis.match('*.css', {
     useSprite: true
 });
 
-/**
- * 模块化开发
- */
-//编译工具扩展：根据不同前端模块化框架，扩展声明依赖能力
-//seajs使用命令，模块化必须指令
-
-// 静态资源管理：解析静态资源映射表加载页面用到的组件及其组件的依赖
-fis.match('::packager', {
-    postpackager: fis.plugin('loader')
-});
-// 目录规范：设置某个文件夹下资源标记为依赖
-fis.match('**/use-seajs/**.js', {
-    isMod: true
-});
-
-fis.match('/demo/**sea.js', {
-    isMod: false
-});
-
-fis.hook('cmd', {
-    baseUrl: '/demo/use-seajs/sea-modules/',
-
-    paths: {
-        "jquery": "jquery/jquery/1.10.1/jquery.js",
-        "$": "jquery/jquery/1.10.1/jquery.js",
-        "jquery-easing": "jquery/easing/1.3.0/easing.js",
-        "store": "gallery/store/1.3.7/store",
-        "angularjs": "angular/angularjs/1.1.5/angular.js",
-        "underscore": "gallery/underscore/1.4.4/underscore.js",
-        "backbone": "gallery/backbone/1.0.0/backbone.js"
-    }
-});
+// /**
+//  * 模块化开发
+//  */
+// //编译工具扩展：根据不同前端模块化框架，扩展声明依赖能力
+// //seajs使用命令，模块化必须指令
+//
+// // 静态资源管理：解析静态资源映射表加载页面用到的组件及其组件的依赖
+// fis.match('::packager', {
+//     postpackager: fis.plugin('loader')
+// });
+// // 目录规范：设置某个文件夹下资源标记为依赖
+// fis.match('**/use-seajs/**.js', {
+//     isMod: true
+// });
+//
+// fis.match('/demo/**sea.js', {
+//     isMod: false
+// });
+//
+// fis.hook('cmd', {
+//     baseUrl: '/demo/use-seajs/sea-modules/',
+//
+//     paths: {
+//         "jquery": "jquery/jquery/1.10.1/jquery.js",
+//         "$": "jquery/jquery/1.10.1/jquery.js",
+//         "jquery-easing": "jquery/easing/1.3.0/easing.js",
+//         "store": "gallery/store/1.3.7/store",
+//         "angularjs": "angular/angularjs/1.1.5/angular.js",
+//         "underscore": "gallery/underscore/1.4.4/underscore.js",
+//         "backbone": "gallery/backbone/1.0.0/backbone.js"
+//     }
+// });
 /**
  * css雪碧图支持
  * 启用打包插件，必须匹配 ::package
@@ -72,6 +72,31 @@ fis.match('::package', {
         layout: 'matrix',
         margin: '15',
         htmlUseSprite: true // 内联样式使用css雪碧图
+    })
+});
+
+/**
+ * 打包
+ * 默认插件fis3-packager-map
+ */
+//
+// fis.match('/js/*.js',{
+//    packTo:'/combine/pkg.js'
+// });
+//
+//
+// // 调整打包顺序
+// fis.match('/js/b.js',{
+//     packOrder: -10
+// });
+//更简洁的打包顺序
+
+fis.match('::package', {
+    packager: fis.plugin('map', {
+        '/combine/pkg.js': [
+            '/js/b.js',
+            '/js/*.js'
+        ]
     })
 });
 
---
+++ src/js/b.js
@@ -0,0 +1,4 @@
+/**
+ * Created by linguochi(382150914@qq.com) on 12-6 0006.
+ */
+console.log('i am b.js');
